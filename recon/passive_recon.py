05850fad20654efbdfd1ae2a43af111d6666e4fa
codesecurely <marek.kaliszczyk@gmail.com>
2022-05-16 03:47:51 +0200

    passive recon script resolvers

--- recon/passive_recon.py
+++ recon/passive_recon.py
@@ -39,8 +39,7 @@ def find_A_records(file, target):
                 ips.append(line.split()[2])
     with open('resolved-'+target+'.txt', 'w') as A_records:
         for ip in ips:
-            A_records.write(ip)
-            A_records.write('\n')
+            A_records.write(ip+'\n')
 
 
 def run_smap(target, targets):
@@ -63,19 +62,35 @@ def check_dependencies():
         exit(1)
 
 
+def prepare_resolvers():
+    with open('resolvers.txt', 'w') as resolvers:
+        resolvers.write('1.0.0.1'+'\n')
+        resolvers.write('8.8.8.8'+'\n')
+        resolvers.write('208.67.220.222'+'\n')
+        resolvers.write('198.82.247.34'+'\n')
+
+
 def main(argv):
     parser = argparse.ArgumentParser(
-        description="Perform basic recon using only passive techniques\ntools: amass, massdns, smap")
-    parser.add_argument('--domain', required=True, help="domain for amass, example.com")
-    parser.add_argument('--resolvers', required=True, help="a file with resolvers in each line for massdns")
+        description="Perform basic recon using only passive techniques, tools: amass, massdns, smap")
+    parser.add_argument('--domain', required=True,
+                        help="domain for amass, example.com")
+    parser.add_argument('--resolvers', required=False,
+                        help="a file with resolvers in each line for massdns, optional")
     args = parser.parse_args()
 
     check_dependencies()
 
+    if not args.resolvers:
+        prepare_resolvers()
+        resolvers = 'resolvers.txt'
+
+    resolvers = args.resolvers
+
     if not os.path.isfile('amass-'+args.domain+'.txt'):
         run_amass(args.domain)
     if not os.path.isfile('massdns-'+args.domain+'.txt'):
-        run_massdns(args.domain, args.resolvers)
+        run_massdns(args.domain, resolvers)
     if os.path.isfile('massdns-'+args.domain+'.txt'):
         find_A_records('massdns-'+args.domain+'.txt', args.domain)
     if os.path.isfile('resolved-'+args.domain+'.txt'):
